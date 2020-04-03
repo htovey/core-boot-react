5cb8146d31bef427bcfc8a9955c1170d00720633
Heather Tovey <heather.e.tovey@gmail.com>
2020-04-03 11:20:13 -0400

    ghpages2

---
+++ build/precache-manifest.ba1b613057f5a064144ca11ff52540ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f1017f9aa1d789614135eceaf842db",
+    "url": "/core-boot-react/index.html"
+  },
+  {
+    "revision": "a15fa9a24b3af66428d1",
+    "url": "/core-boot-react/static/css/main.aa8b3375.chunk.css"
+  },
+  {
+    "revision": "553a4735bf7a9ed4a295",
+    "url": "/core-boot-react/static/js/2.755e7640.chunk.js"
+  },
+  {
+    "revision": "a15fa9a24b3af66428d1",
+    "url": "/core-boot-react/static/js/main.d87dfb35.chunk.js"
+  },
+  {
+    "revision": "bd874e4d173b6a3b59e7",
+    "url": "/core-boot-react/static/js/runtime-main.23711846.js"
+  }
+]);
